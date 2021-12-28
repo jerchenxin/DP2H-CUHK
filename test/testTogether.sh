c946fdfbb591e5d5c80a424386ca77a6731cf4cf
jerchenxin <jerchenxin@gmail.com>
2021-12-28 11:24:01 +0800

    add arg base num

--- test/testTogether.sh
+++ test/testTogether.sh
@@ -1,3 +1,3 @@
 cd ../build
 
-stdbuf -o 0 ./DP2H --filePath=../test/$1 --benchmark=together --loadBinary=true --round=$2  --showLabelNum=true > $1.together.$2.binary
+stdbuf -o 0 ./DP2H --filePath=../test/$1 --benchmark=together --loadBinary=true --round=$2 --num=$3 --showLabelNum=true > $1.together.binary
