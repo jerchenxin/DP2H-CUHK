93c6c1f42464461b17d4afbede3d0244ca49c1f0
jerchenxin <jerchenxin@gmail.com>
2022-01-16 19:27:12 +0800

    change method

---
+++ LargeCutLabel.py
@@ -0,0 +1,44 @@
+import random
+import sys
+
+in_file = sys.argv[1]
+
+graph = {}
+
+with open(in_file, "r") as f:
+    f.readline()
+    
+    line = f.readline()
+    while line:
+        tmp = line.split()
+        
+        u = int(tmp[0])
+        v = int(tmp[1])
+        label = int(tmp[2])
+        
+        if not graph.get(label):
+            graph[label] = [(u, v), ]
+        else:
+            graph[label].append((u, v))
+        
+        line = f.readline()
+
+keys = graph.keys()
+key_num = len(keys)
+
+labelSet = set()
+
+while len(labelSet) < 500:
+    labelSet.add(random.randint(0, key_num-1))
+
+label_list = list(labelSet)
+random.shuffle(label_list)
+
+for num in range(0, 5):
+    with open(in_file + ".cut." + str(num), "w") as f:
+        for index in range(100*num, 100*(num+1)):
+            i = label_list[index]
+            for j in graph[i]:
+                f.write("{} {} {}\n".format(j[0], j[1], i))
+
+
